6c4be9d467dd04dd3ff7089a54bed710449a06fe
Nanami <ku_kai73@yahoo.co.jp>
2016-10-12 19:03:34 +1000

    second

--- app/src/main/java/jp/techacademy/nanami/yeates/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/nanami/yeates/javalog/MainActivity.java
@@ -1,7 +1,7 @@
 package jp.techacademy.nanami.yeates.javalog;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,12 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Human human = new Human("ともこ", 16);
+        Human human = new Human("ともこ",16,"読書");
 
         human.say();
         Log.d("javatest", "「私の名前は" + human.name + "です。年は" + human.age + "歳です。」");
 
-        Human human2 = new Human("読書");
+        Human human2 = new Human("ともこ",16,"読書");
 
         human2.think();
     }
